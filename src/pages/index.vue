6f6c829dd2fcd112c1b8ef03f4fc66563984bb96
gitNimad <n.marashi.info@gmail.com>
2024-05-30 15:29:03 +0200

    <layout> modify default layout to a preset of vuetify - TODO in commit body =>
    copy+paste from https://vuetifyjs.com/en/features/application-layout/
    basic simple layout
    TODO main component -> ProductTable
    TODO nav-bar
    TODO nav-drawer for small Displays

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <HelloWorld />
+  <v-layout class="rounded rounded-md">
+    <v-app-bar title="Application bar"></v-app-bar>
+
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+      <ProductTable />
+    </v-main>
+  </v-layout>
 </template>
 
 <script lang="ts" setup>
