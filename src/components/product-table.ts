6ef5b710b6b32cada0f5093522b0ed32c68d8284
gitNimad <n.marashi.info@gmail.com>
2024-06-02 15:31:39 +0200

    <product-table.ts> create async loadProducts()  to fetch data from path /products and assign to products.value

--- src/components/product-table.ts
+++ src/components/product-table.ts
@@ -3,3 +3,7 @@ import { FetchApi } from '@/http/fetchApi';
 
 const fetchApi = new FetchApi("https://stock-manager.hooman.de/api/");
 const products = ref([]);
+
+async function loadProducts() {
+    products.value = await fetchApi.requestGet("products");
+}
