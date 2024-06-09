793cb6a80437f241a2a40c9298557ff176e5479d
gitNimad <n.marashi.info@gmail.com>
2024-06-09 14:26:48 +0200

    <product-table.vue><add> <v-alert> for visual error-feedback on website, if fetching fails
    <add> v-if="error" if error is not null anymore and has err.message value,
    also see product-table.ts
    
    <add> type="error" for red-color and visual feedback of an error
    
    <add> closable prop to enable user to close the error message
    
    <also> usage of vetur auto-formatting

--- src/components/product-table.vue
+++ src/components/product-table.vue
@@ -1,7 +1,14 @@
 <template>
-    <v-container class="main-container-table fill-height">
-        <v-data-table :loading="isLoading" :items="products" show-select></v-data-table>
-    </v-container>
+  <v-container class="main-container-table fill-height">
+    <v-data-table
+      :loading="isLoading"
+      :items="products"
+      show-select
+    ></v-data-table>
+    <v-alert v-if="error" type="error" closable>
+      {{ error }}
+    </v-alert>
+  </v-container>
 </template>
 
 <script src="./product-table" lang="ts"></script>
