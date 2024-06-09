a04eb5cb123d9c1c5608e8370dad3bed7f4b00f7
gitNimad <n.marashi.info@gmail.com>
2024-06-09 14:41:15 +0200

    <product-table.ts><change> the ref([]) assignment-type of products from any to generic <[Products]>
    The fetched data, products must be an Array of type Products -> products-interface.ts

--- src/components/product-table.ts
+++ src/components/product-table.ts
@@ -1,10 +1,11 @@
 import { defineComponent, onBeforeMount, ref } from "vue";
 import { FetchApi } from "@/http/fetchApi";
+import Product from "./products-interface";
 
 export default defineComponent({
   setup() {
     const fetchApi = new FetchApi("https://stock-manager.hooman.de/api/");
-    const products = ref([]);
+    const products = ref<[Product]>();
     const isLoading = ref(false);
     const error = ref<string | null>(null);
 
