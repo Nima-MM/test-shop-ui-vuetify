a0d1afb070d580e59f6fe231a0a911c7625f49a3
gitNimad <n.marashi.info@gmail.com>
2024-06-09 13:39:10 +0200

    <product-table><create> ref() error of type<string | null>(null) for visual feedback in the VDOM

--- src/components/product-table.ts
+++ src/components/product-table.ts
@@ -1,29 +1,28 @@
-import { defineAsyncComponent, defineComponent, onBeforeMount, ref } from 'vue';
-import { FetchApi } from '@/http/fetchApi';
-import { error } from 'console';
-
+import { defineComponent, onBeforeMount, ref } from "vue";
+import { FetchApi } from "@/http/fetchApi";
 
 export default defineComponent({
-    setup() {
-        const fetchApi = new FetchApi("https://stock-manager.hooman.de/api/");
-        const products = ref([]);
-        const isLoading = ref(false);
+  setup() {
+    const fetchApi = new FetchApi("https://stock-manager.hooman.de/api/");
+    const products = ref([]);
+    const isLoading = ref(false);
+    const error = ref<string | null>(null);
 
-        onBeforeMount(() => {
-            loadProducts();
-        })
+    onBeforeMount(() => {
+      loadProducts();
+    });
 
-        async function loadProducts() {
-            isLoading.value = true;
-            try{
-                products.value = await fetchApi.requestGet("products");
-            } catch(err) {
-                console.error("Fetching products failed. ERR: ", err)
-            } finally {
-                isLoading.value = false;
-            }
-        }
-
-        return { products, isLoading };
+    async function loadProducts() {
+      isLoading.value = true;
+      try {
+        products.value = await fetchApi.requestGet("products");
+      } catch (err) {
+        console.error("Fetching products failed. ERR: ", err);
+      } finally {
+        isLoading.value = false;
+      }
     }
-})
+
+    return { products, isLoading };
+  },
+});
