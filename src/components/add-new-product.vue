44972c4894e7952281321f38f746537ed4683dc5
gitNimad <n.marashi.info@gmail.com>
2024-08-22 13:53:54 +0200

    <component><add-new-product><add> simple basic v-card in the v-dialog window

--- src/components/add-new-product.vue
+++ src/components/add-new-product.vue
@@ -1,6 +1,11 @@
 <template>
 
-  <v-dialog v-model="dialog" max-width="500">DIALOG</v-dialog>
+  <v-dialog v-model="dialog" max-width="500">
+    <v-card max-width="400">
+      <v-card-title><span>CREATE NEW PRODUCT</span></v-card-title>
+      <v-card-text>TEXT TEXT</v-card-text>
+    </v-card>
+  </v-dialog>
   <v-btn class="mb-2" color="primary" dark @click="dialog = true">
     new producta
   </v-btn>
