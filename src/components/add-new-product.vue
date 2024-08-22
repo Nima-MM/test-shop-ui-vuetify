b67288f3dcd0d9faded61c0715790ee7fab5ef44
gitNimad <n.marashi.info@gmail.com>
2024-08-22 14:21:33 +0200

    <component><add-new-product><add> simple basic v-card-title and v-text-input-field in the v-dialog window

--- src/components/add-new-product.vue
+++ src/components/add-new-product.vue
@@ -3,7 +3,20 @@
   <v-dialog v-model="dialog" max-width="500">
     <v-card max-width="400">
       <v-card-title><span>CREATE NEW PRODUCT</span></v-card-title>
-      <v-card-text>TEXT TEXT</v-card-text>
+      <v-card-text>
+        <v-container>
+          <v-col>
+            <v-text-field placeholder="Produktname">
+              
+            </v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field placeholder="Stückzahl">
+              
+            </v-text-field>
+          </v-col>
+        </v-container>
+      </v-card-text>
     </v-card>
   </v-dialog>
   <v-btn class="mb-2" color="primary" dark @click="dialog = true">
