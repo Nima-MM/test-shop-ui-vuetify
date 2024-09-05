734e13bf4767010a0f59373789bc70fc7f226744
gitNimad <marashi.nima@outlook.de>
2024-09-05 21:00:45 +0200

    <update><add-new-product.ts> add productStore from store into this comp. also...
    ... use the addProduct-method to add the new typed product also into the productStore
    and not only to backend.
    
    ... add a watcher to make sure, the fetched data stored into productStore is loading first.

--- src/components/add-new-product.ts
+++ src/components/add-new-product.ts
@@ -1,6 +1,7 @@
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, watch } from "vue";
 import { FetchApi } from "../http/fetchApi";
 import Product from "./products-interface";
+import { useProductsStore } from "../stores/all-products-store";
 
 export default defineComponent({
   setup() {
@@ -9,6 +10,16 @@ export default defineComponent({
     const error = ref<string | null>(null);
     const dialog = ref(false);
     const product = ref<Product>({ name: "" });
+    const productStore = useProductsStore();
+
+    // Watcher to look when products are fetched into the product-table
+    watch(
+      () => productStore.getState,
+      (newProducts) => {
+        console.log("Products updated: ", newProducts);
+      },
+      { immediate: false }
+    );
 
     const save = async function (): Promise<void> {
       try {
@@ -16,6 +27,7 @@ export default defineComponent({
           "/products/add",
           product.value
         );
+        productStore.addProduct(product.value);
       } catch (err) {
         error.value = `POST producta failed: Error: ${err.message}`;
         console.error("Error details: ", err.value);
