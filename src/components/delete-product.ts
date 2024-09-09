d0b76dcee116d27d040353d506bce46417efa0e6
gitNimad <marashi.nima@outlook.de>
2024-09-09 14:50:52 +0200

    STORY-10 <create><delete-product.ts> create a del function - yet to refactor
    the del function deletes as supposed but it is not reactive yet

--- src/components/delete-product.ts
+++ src/components/delete-product.ts
@@ -2,7 +2,34 @@ import { defineComponent, ref, watch } from "vue";
 import { FetchApi } from "../http/fetchApi";
 import Product from "./products-interface";
 import { useProductsStore } from "../stores/all-products-store";
+import { RefSymbol } from "@vue/reactivity";
 
 export default defineComponent({
-  setup() {},
+  setup() {
+    const fetchApi = new FetchApi(
+      "https://stock-manager.hooman.de/api/products/"
+    );
+    const error = ref<string | null>(null);
+    const response = ref<any>(null);
+    const id = ref<number>();
+    const dialog = ref(false);
+
+    const del = async () => {
+      if (id.value !== null || id.value !== undefined) {
+        try {
+          // TO-REFACTOR - wants init value - yet to fix - STORY 10 - but actualy works...
+          response.value = await fetchApi.requestDel(id.value);
+        } catch (err) {
+          error.value = `Deleting prdouct failed: Error: ${err.message}`;
+          console.error(error.value);
+        } finally {
+          dialog.value = false;
+          console.log(response.value);
+        }
+      } else {
+        console.error("AHHHHHHHH");
+      }
+    };
+    return { id, dialog, del };
+  },
 });
