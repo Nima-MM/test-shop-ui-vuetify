798720433318a8bb96bc161c4cc1faea34eb1b24
gitNimad <marashi.nima@outlook.de>
2024-09-09 12:50:27 +0200

    STORY-10 <create><delete-product.vue> create delete-btn with no func/dialog yet
    create btn:
    <v-btn class="mb-2" color="primary" dark @click="dialog = true">
        Delete Product
    </v-btn>
    
    no dialog nor func yet

--- src/components/delete-product.vue
+++ src/components/delete-product.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <v-btn class="mb-2" color="primary" dark @click="dialog = true">
+    Delete Product
+  </v-btn>
 </template>
 
 <script src="./delete-product" lang="ts"></script>
